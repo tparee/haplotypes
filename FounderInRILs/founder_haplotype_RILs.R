c7333165a22543d31746e5496cee5c49c8b75976
tparee <60737809+tparee@users.noreply.github.com>
2024-04-02 16:08:10 -0400

    Update founder_haplotype_RILs.R

--- FounderInRILs/founder_haplotype_RILs.R
+++ FounderInRILs/founder_haplotype_RILs.R
@@ -7,7 +7,8 @@ source("/Users/tomparee/Desktop/happlotype_TUC/Functions_SearchFounderHaplo.R")
 #########################################
 ######### Impor haplotypes #############
 
-
+#################################
+#### For tuc's ceMEE RILS #######
 genomatrix = read_tsv("~/Desktop/happlotype_TUC/Genotype_Matrix.tsv")
 genomatrix = as.data.frame(genomatrix)
 genomatrix = subset(genomatrix, CHROM == "I")
@@ -31,6 +32,31 @@ rils = genomatrix[,!(colnames(genomatrix) %in% foundernames)]
 # Let's do a subset of 15 rils for example to reduce the computing time
 #rils = rils[,sample(1:ncol(rils), 15)]
 
+##################################
+##### For Jose's becei rils ########
+
+#rils = read_csv("/Users/tomparee/Documents/Documents - MacBook Pro de tom/rockmanlab/cbecei_haplo/becei_chr_I_Cross_A_filtered_genotype_table.csv")
+#rils = rils[,7:154]
+#rilsname = colnames(rils)
+#rils[rils=="1/1"] = "1"
+#rils[rils=="0/0"] = "0"
+#rils[rils=="0/1"]="0.5"
+#rils = matrix(as.numeric(unlist(c(rils))), ncol=ncol(rils))
+#colnames(rils)=rilsname
+
+#load("/Users/tomparee/Documents/Documents - MacBook Pro de tom/rockmanlab/cbecei_haplo/founderHaplotypesCrossA.Rdata")
+#founders = founderCrossA
+#snps = founders[,1:4]
+#colnames(snps)=tolower(colnames(snps))
+#founders=founders[,5:8]
+#founders = matrix(as.numeric(unlist(c(founders))), ncol=ncol(founders))
+#colnames(founders)=colnames(founderCrossA)[5:8]
+
+#fixed = apply(rils, 1, function(x){x = mean(x, na.rm=T); x %in% c(0,1)})
+#snps = snps[!fixed,]
+#rils = rils[!fixed,]
+#founders = founders[!fixed,]
+
 
 #######################################
 ############## RUN ####################
@@ -40,13 +66,18 @@ rils = genomatrix[,!(colnames(genomatrix) %in% foundernames)]
 
 #####################################
 #### Call haplotype for the lines ###
-haplotype = do.call(rbind,lapply(1:ncol(rils), function(thisril){
+#ix = 1:ncol(rils)
+haplotype = do.call(rbind,lapply(ix, function(thisril){
   print(thisril)
   ril = rils[,thisril]
   ril[ril!=1 & ril!=0]=NA
-  output = haplosearch(ril, founders, snps)
-  output$rilname = thisril
-  output
+  output = try(haplosearch(ril, founders, snps))
+  if(class(output)!='try-error'){
+    output$rilname = colnames(rils)[thisril]
+    return(output)
+  }else{
+    return(NULL)
+  }
 }))
 
 haplotype=haplotype[!is.na(haplotype[,3]),]
